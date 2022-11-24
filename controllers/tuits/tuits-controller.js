a33d4ba9e7e8cf2e6d0e479aba6202d4481f8ee4
obreese <breese.o@northeastern.edu>
2022-11-23 18:57:22 -0800

    fix typo and uncomment response statuses

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -47,16 +47,16 @@ const findTuits = async (req, res) => {
   res.json(tuits);
 };
 const updateTuit = async (req, res) => {
-  const tuitdIdToUpdate = req.params.tid;
+  const tuitIdToUpdate = req.params.tid;
   const updates = req.body;
   const status = await tuitsDao.updateTuit(tuitIdToUpdate, updates);
-  // res.sendStatus(status);
+  res.sendStatus(status);
 };
 
 const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
   const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-  // res.sendStatus(status);
+  res.sendStatus(status);
 };
 
 export default (app) => {
